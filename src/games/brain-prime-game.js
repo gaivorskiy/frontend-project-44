0de5d0f64e350801fb21963593a1c329c94683f8
Gaivorskiy <gaivorskiy@gmail.com>
2022-09-25 19:58:14 +0300

    first engine 0.0.17

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -14,7 +14,6 @@ const prime = () => {
       for (let i = 2; i <= Math.sqrt(n); i += 1) {
         if (n % i !== 0) {
           result = 'yes';
-          return result;
         }
         if (n % i === 0) {
           result = 'no';
